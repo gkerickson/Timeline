f800c2686f007dbc85d46541a986a4a132c548a5
Galen Erickson <gkerickson@wisc.edu>
2021-02-21 17:04:35 -0600

    Add defaults to query

--- app/src/main/java/com/erickson/timeline/smithsonian/Smithsonian.kt
+++ app/src/main/java/com/erickson/timeline/smithsonian/Smithsonian.kt
@@ -7,5 +7,10 @@ import retrofit2.http.Query
 
 interface Smithsonian {
     @GET("category/art_design/search")
-    fun getIds(@Query("api_key") key: String, @Query("q") query: String): Call<RequestDefinitions.Body<RequestDefinitions.SearchData>>
+    fun getIds(
+        @Query("api_key") key: String = API_KEY,
+        @Query("q") query: String = QUERY,
+        @Query("rows") rows: Int = 10,
+        @Query("start") start: Int = 0
+    ): Call<RequestDefinitions.Body<RequestDefinitions.SearchData>>
 }
