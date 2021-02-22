2cb252cccaf0b6cc005decefb05793225de4c229
Galen Erickson <gkerickson@wisc.edu>
2021-02-22 16:53:58 -0600

    Use AnimationDelegate to clean up activity

--- app/src/main/java/com/erickson/timeline/AnimationDelegate.kt
+++ app/src/main/java/com/erickson/timeline/AnimationDelegate.kt
@@ -1,4 +1,112 @@
 package com.erickson.timeline
 
-class AnimationDelegate {
+import android.animation.ValueAnimator
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Guideline
+import kotlin.math.abs
+import androidx.core.view.setPadding
+import com.erickson.timeline.MainActivity.Companion.imageIds
+
+class AnimationDelegate(
+    private val activity: MainActivity
+) {
+
+    private val timelineImageViews: List<ImageView> by lazy {
+        imageIds.map { activity.findViewById(it) }
+    }
+
+    private val choiceImageViews: List<ImageView> by lazy {
+        listOf(
+            activity.findViewById(R.id.selection_one),
+            activity.findViewById(R.id.selection_two)
+        )
+    }
+
+    fun toTimelineFocus() {
+        activity.findViewById<View>(R.id.timeline_layout).apply {
+            isClickable = false
+        }
+        timelineImageViews.forEach {
+            it.isClickable = true
+        }
+        choiceImageViews.forEach {
+            it.isClickable = false
+        }
+        activity.findViewById<View>(R.id.parent_layout).apply {
+            isClickable = true
+        }
+        animator.start()
+    }
+
+    fun toChoiceFocus() {
+        activity.findViewById<View>(R.id.timeline_layout).apply {
+            isClickable = true
+        }
+        timelineImageViews.forEach {
+            it.isClickable = false
+        }
+        choiceImageViews.forEach {
+            it.isClickable = true
+        }
+        activity.findViewById<View>(R.id.parent_layout).apply {
+            isClickable = false
+        }
+        animator.reverse()
+    }
+
+    interface ToAnimate {
+        val start: Float
+        val end: Float
+        fun setValue(value: Float)
+    }
+
+    private val animationList: List<ToAnimate> = listOf(
+        object : ToAnimate {
+            override val start: Float = 0.75f
+            override val end: Float = 0.25f
+            override fun setValue(value: Float) {
+                (activity.findViewById<Guideline>(R.id.main_guideline).layoutParams as ConstraintLayout.LayoutParams).apply {
+                    guidePercent = value
+                    activity.findViewById<Guideline>(R.id.main_guideline).layoutParams = this
+                }
+            }
+        },
+        object : ToAnimate {
+            override val start: Float = 0f
+            override val end: Float = 10f
+            override fun setValue(value: Float) {
+                timelineImageViews.forEach {
+                    it.setPadding(value.toInt())
+                }
+            }
+        },
+        object : ToAnimate {
+            override val start: Float = 50f
+            override val end: Float = 0f
+            override fun setValue(value: Float) {
+                choiceImageViews.forEach {
+                    it.setPadding(value.toInt())
+                }
+            }
+        }
+    )
+
+    private val animator = ValueAnimator.ofInt(0, 100).apply {
+        duration = 1000
+        interpolator = AccelerateDecelerateInterpolator()
+        addUpdateListener { valueAnimator ->
+            animationList.forEach {
+                it.setValue(
+                    (abs(it.start - it.end) * valueAnimator.animatedFraction).run {
+                        if (it.start > it.end) it.start - this
+                        else it.end - this
+                    }
+                )
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/erickson/timeline/MainActivity.kt
+++ app/src/main/java/com/erickson/timeline/MainActivity.kt
@@ -1,167 +1,81 @@
 package com.erickson.timeline
 
-import android.animation.ValueAnimator
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.Guideline
-import androidx.core.view.setPadding
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import com.erickson.timeline.model.ActiveViewData
 import com.erickson.timeline.model.DataViewModel
-import com.squareup.picasso.Picasso
 import java.util.*
-import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val imageIds = listOf(
+            R.id.timeline_image_1,
+            R.id.timeline_image_2,
+            R.id.timeline_image_3,
+            R.id.timeline_image_4,
+        )
+    }
+
+    private val viewModel: DataViewModel by lazy {
+        ViewModelProvider(this).get(DataViewModel::class.java)
+    }
+
+    private val animator: AnimationDelegate by lazy {
+        AnimationDelegate(this)
+    }
+
     private fun onImageClick() {
         this.supportFragmentManager.beginTransaction()
             .setReorderingAllowed(true)
             .addToBackStack(null)
-            .setCustomAnimations(R.anim.slide_in, R.anim.slide_out,R.anim.slide_in, R.anim.slide_out)
+            .setCustomAnimations(
+                R.anim.slide_in,
+                R.anim.slide_out,
+                R.anim.slide_in,
+                R.anim.slide_out
+            )
             .add(R.id.fragment, DetailViewFragment())
             .commit()
     }
 
-    private val imageIds = listOf(
-        R.id.timeline_image_1,
-        R.id.timeline_image_2,
-        R.id.timeline_image_3,
-        R.id.timeline_image_4,
-    )
-
-    private val timelineImageViews: List<ImageView> by lazy {
-        imageIds.map { findViewById(it) }
+    private fun setupImageLoadIntoViewFromLiveData(
+        liveData: LiveData<ActiveViewData>,
+        view: ImageView,
+        selectedId: MutableLiveData<String>
+    ) {
+        liveData.observe(this) {
+            view.setImageBitmap(it.bitmap)
+            view.rootView.invalidate()
+        }
+        view.isClickable = true
+        view.setOnClickListener {
+            liveData.value?.viewData?.id?.apply {
+                selectedId.value = this
+            }
+            onImageClick()
+        }
     }
 
-    private val choiceImageViews: List<ImageView> by lazy {
-        listOf(
-            findViewById(R.id.selection_one),
-            findViewById(R.id.selection_two)
-        )
-    }
-
-
-    // TODO: Use scenes to manage animation
-    interface ToAnimate {
-        val start: Float
-        val end: Float
-        fun setValue(value: Float)
-    }
-    private val animationList: List<ToAnimate> = listOf(
-        object : ToAnimate {
-            override val start: Float = 0.75f
-            override val end: Float = 0.25f
-            override fun setValue(value: Float) {
-                (findViewById<Guideline>(R.id.main_guideline).layoutParams as ConstraintLayout.LayoutParams).apply {
-                    guidePercent = value
-                    findViewById<Guideline>(R.id.main_guideline).layoutParams = this
-                }
-            }
-        },
-        object : ToAnimate {
-            override val start: Float = 0f
-            override val end: Float = 10f
-            override fun setValue(value: Float) {
-                timelineImageViews.forEach {
-                    it.setPadding(value.toInt())
-                }
-            }
-        },
-        object : ToAnimate {
-            override val start: Float = 50f
-            override val end: Float = 0f
-            override fun setValue(value: Float) {
-                choiceImageViews.forEach {
-                    it.setPadding(value.toInt())
-                }
-            }
-        }
-    )
-    private val animator = ValueAnimator.ofInt(0, 100).apply {
-        duration = 1000
-        interpolator = AccelerateDecelerateInterpolator()
-        addUpdateListener { valueAnimator ->
-            animationList.forEach {
-                it.setValue(
-                    (abs(it.start - it.end) *valueAnimator.animatedFraction).run {
-                        if(it.start > it.end) it.start - this
-                        else it.end - this
-                    }
-                )
-            }
-        }
-    }
-    private fun toTimelineFocus() {
-        findViewById<View>(R.id.timeline_layout).apply {
-            isClickable = false
-        }
-        timelineImageViews.forEach {
-            it.isClickable = true
-        }
-        choiceImageViews.forEach {
-            it.isClickable = false
-        }
-        findViewById<View>(R.id.parent_layout).apply {
-            isClickable = true
-        }
-        animator.start()
-
-    }
-    private fun toChoiceFocus() {
-        findViewById<View>(R.id.timeline_layout).apply {
-            isClickable = true
-        }
-        timelineImageViews.forEach {
-            it.isClickable = false
-        }
-        choiceImageViews.forEach {
-            it.isClickable = true
-        }
-        findViewById<View>(R.id.parent_layout).apply {
-            isClickable = false
-        }
-        animator.reverse()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        Picasso.get().isLoggingEnabled = true
-
-        val viewModel = ViewModelProvider(this).get(DataViewModel::class.java)
-
-        fun setupImageObserver(liveData: LiveData<ActiveViewData>, view: ImageView) {
-            liveData.observe(this) {
-                Log.e("GALEN", "${it.viewData} with ${it.bitmap != null} in ${view.id}")
-                view.setImageBitmap(it.bitmap)
-                view.rootView.invalidate()
-            }
-            view.isClickable = true
-            view.setOnClickListener {
-                liveData.value?.viewData?.id?.apply {
-                    viewModel.selectedId.value = this
-                }
-                onImageClick()
-            }
-        }
-
+    private fun setupImages() {
         for (i in 0 until 4)
             viewModel.timelineViewData.getOrNull(i)?.let {
-                setupImageObserver(it, findViewById(imageIds[i]))
+                setupImageLoadIntoViewFromLiveData(it, findViewById(imageIds[i]), viewModel.selectedId)
             }
-        setupImageObserver(viewModel.choiceOneViewData, findViewById(R.id.selection_one))
-        setupImageObserver(viewModel.choiceTwoViewData, findViewById(R.id.selection_two))
-        findViewById<View>(R.id.timeline_layout).setOnClickListener { toTimelineFocus() }
-        findViewById<View>(R.id.parent_layout).setOnClickListener { toChoiceFocus() }
+        setupImageLoadIntoViewFromLiveData(viewModel.choiceOneViewData, findViewById(R.id.selection_one), viewModel.selectedId)
+        setupImageLoadIntoViewFromLiveData(viewModel.choiceTwoViewData, findViewById(R.id.selection_two), viewModel.selectedId)
 
+        findViewById<View>(R.id.timeline_layout).setOnClickListener { animator.toTimelineFocus() }
+        findViewById<View>(R.id.parent_layout).setOnClickListener { animator.toChoiceFocus() }
+
+    }
+
+    private fun setupTimelineYears() {
         viewModel.highestTime.observe(this) {
             findViewById<TextView>(R.id.topTime).apply {
                 text = Calendar.getInstance().run {
@@ -171,6 +85,7 @@ class MainActivity : AppCompatActivity() {
                 invalidate()
             }
         }
+
         viewModel.lowestTime.observe(this) {
             findViewById<TextView>(R.id.bottomTime).apply {
                 text = Calendar.getInstance().run {
@@ -180,6 +95,13 @@ class MainActivity : AppCompatActivity() {
                 invalidate()
             }
         }
-        toTimelineFocus()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupImages()
+        setupTimelineYears()
+        animator.toTimelineFocus()
     }
 }
